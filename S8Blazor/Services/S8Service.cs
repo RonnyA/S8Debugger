b16ed9bbe3ab0af7d3cf552e54139318e188e70d
Ronny Hansen <ronnyhan@gmail.com>
2020-12-27 14:41:34 +0100

    First draft of Blazor support

---
+++ S8Blazor/Services/S8Service.cs
@@ -0,0 +1,28 @@
+﻿using S8Debugger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S8Blazor.Services
+{
+    public class S8Service : IS8Service
+    {
+        S8CommandParser parser;
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public S8Service()
+        {
+            parser = new S8CommandParser();
+        }
+
+        public Task<bool> Run()
+        {
+            parser.ParseCommand("RUN");
+
+            return Task.FromResult(true);
+
+        }
+    }
+}
