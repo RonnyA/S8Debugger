87e3f8b0417d2ec1d14ec9d51cc2a42923a9b796
Ronny Hansen <ronnyhan@gmail.com>
2021-01-03 20:43:44 +0100

    Added download functionality for binary s8 files typescripåt/javascript helper function from https://github.com/sample-by-jsakamoto/Blazor-DownloadFIles

---
+++ S8Blazor/TypeScript/downloadHelper.ts
@@ -0,0 +1,18 @@
+﻿function clickElement(element: HTMLElement): void {
+    element.click();
+}
+
+function downloadFromUrl(options: { url: string, fileName?: string }): void {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = options.url;
+    anchorElement.download = options.fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+}
+
+function downloadFromByteArray(options: { byteArray: string, fileName: string, contentType: string }): void {
+    // The byte array in .NET is encoded to base64 string when it passes to JavaScript.
+    // So we can pass that base64 encoded string to the browser as a "data URL" directly.
+    const url = "data:" + options.contentType + ";base64," + options.byteArray;
+    downloadFromUrl({ url: url, fileName: options.fileName });
+}
